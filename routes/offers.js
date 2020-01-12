c4f33a496e63e056ff02d049d2799fa374c58180
HugoLattard <55689599+Orphee1@users.noreply.github.com>
2020-01-12 12:04:56 +0100

    offers update

--- routes/offers.js
+++ routes/offers.js
@@ -117,60 +117,6 @@ router.get("/api/offers/with-count", async (req, res) => {
   } catch (e) {
     res.status(400).json({ message: e.message });
   }
-
-  // const filter = {};
-  // if (
-  //   (req.query.priceMin !== undefined && req.query.priceMin !== "") ||
-  //   (req.query.priceMax !== undefined && req.query.priceMax !== "")
-  // ) {
-  //   filter.price = {};
-  //   if (req.query.priceMin) {
-  //     filter.price["$gte"] = req.query.priceMin;
-  //   }
-  //   if (req.query.priceMax) {
-  //     filter.price["$lte"] = req.query.priceMax;
-  //   }
-  // }
-
-  // if (req.query.title) {
-  //   filter.title = {
-  //     $regex: req.query.title,
-  //     $options: "i"
-  //   };
-  // }
-
-  // const query = Offer.find(filter);
-  // console.log(query);
-  // // const query = Offer.find(filter).populate({
-  // //   path: "creator",
-  // //   select: "account"
-  // // });
-
-  // switch (req.query.sort) {
-  //   case "price-desc":
-  //     query.sort({ price: -1 });
-  //     break;
-  //   case "price-asc":
-  //     query.sort({ price: 1 });
-  //     break;
-  //   case "date-desc":
-  //     query.sort({ created: -1 });
-  //     break;
-  //   case "date-asc":
-  //     query.sort({ created: 1 });
-  //     break;
-  //   default:
-  // }
-
-  // let response = {
-  //   count: query.length,
-  //   offers: query.slice(skip, limitOk)
-  // };
-  // await res.json(response);
-
-  // query.exec(function(err, response) {
-  //   res.json(response);
-  // });
 });
 
 module.exports = router;
